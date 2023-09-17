b693d5da7ed97de46cec786566c2733eaa726f0d
Unnikrishnan G <unnikrishnang628@gmail.com>
2023-09-17 20:22:53 +0530

    changed error file to app directory

--- app/error.tsx
+++ app/error.tsx
@@ -10,6 +10,7 @@ export default function Error({error,reset,}: {error: Error ,reset: () => void})
   return (
     <div>
       <h2>Something went wrong!</h2>
+      <h2>{error.message}</h2>
       <button onClick={() => reset()}>Try again</button>
     </div>
   )
