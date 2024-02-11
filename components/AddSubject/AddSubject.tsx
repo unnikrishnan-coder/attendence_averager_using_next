1843bee4b3ebe89c27debf690e51d7fe59d5bfa0
Unnikrishnan G <unnikrishnang628@gmail.com>
2024-02-11 15:33:09 +0530

    chnaged attended and total to number type

--- components/AddSubject/AddSubject.tsx
+++ components/AddSubject/AddSubject.tsx
@@ -6,7 +6,7 @@ import {toast,ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { db } from "@/firebase";
 
-const AddSubject = ({ title, nameVal, attendedVal, totalVal, update, id ,setSubjects}: { title: string, nameVal: string, attendedVal: string, totalVal: string, update?: boolean, id: string ,setSubjects?:Function}) => {
+const AddSubject = ({ title, nameVal, attendedVal, totalVal, update, id ,setSubjects}: { title: string, nameVal: string, attendedVal: number, totalVal: number, update?: boolean, id: string ,setSubjects?:Function}) => {
 
   const [name, setName] = useState(nameVal);
   const [attended, setAttended] = useState(attendedVal);
@@ -15,8 +15,8 @@ const AddSubject = ({ title, nameVal, attendedVal, totalVal, update, id ,setSubj
 
   const resetData = () => {
     setName("");
-    setAttended("");
-    setTotal("");
+    setAttended(0);
+    setTotal(0);
   }
 
   const validate = () : boolean=>{
@@ -28,7 +28,8 @@ const AddSubject = ({ title, nameVal, attendedVal, totalVal, update, id ,setSubj
   };
 
   const updateSubject = ()=>{
-    updateDoc(doc(db, "subjects", id), { name, attended, total }).then((res) => {
+    updateDoc(doc(db, "subjects", id), { name, attended: Number(attended),
+      total: Number(total) }).then((res) => {
       setLoading(false);
       toast("Successfully updated course",{
         position:'top-right',
@@ -63,7 +64,7 @@ const AddSubject = ({ title, nameVal, attendedVal, totalVal, update, id ,setSubj
         const uid = getCookie("user_id");
         addDoc(collection(db, "subjects"), {
           name: name,
-          attended: attended,
+          attended:attended,
           total: total,
           uid: uid
         }).then((res) => {
@@ -101,9 +102,9 @@ const AddSubject = ({ title, nameVal, attendedVal, totalVal, update, id ,setSubj
           <label htmlFor="name" className="mt-3">Course Name</label>
           <input type="text" className='glass-input' value={name} onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder='Enter the course name' required/>
           <label htmlFor="total" className="mt-3">Total Hours</label>
-          <input type="number" className='glass-input' value={total} onChange={(e) => setTotal(e.target.value)} name="total" id="total" placeholder='Enter the total course hours' required/>
+          <input type="number" className='glass-input' value={total} onChange={(e) => setTotal(e.target.valueAsNumber)} name="total" id="total" placeholder='Enter the total course hours' required/>
           <label htmlFor="attended" className="mt-3">Attended Hours</label>
-          <input type="number" name="attended" id="attended" value={attended} onChange={(e) => setAttended(e.target.value)} className='mb-3 glass-input' placeholder='Enter the attended hours' required/>
+          <input type="number" name="attended" id="attended" value={attended} onChange={(e) => setAttended(e.target.valueAsNumber)} className='mb-3 glass-input' placeholder='Enter the attended hours' required/>
           <button type="submit" className='rounded text-white glass-div-front p-3 disabled:cursor-not-allowed' disabled={loading} >{loading ? "Loading..." : "Submit"}</button>
         </form>
       </div>
